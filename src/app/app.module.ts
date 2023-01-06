33e3f116be002c7a326c83ed6c31a891775315af
Oscar2705 <oscarvelasquezl123@gmail.com>
2023-01-05 20:06:44 -0500

    Formulario Sesion12

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'; //Primer video
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-
+import { ContactComponent } from './contact/contact.component';
+import { ContactReactiveComponent } from './contact-reactive/contact-reactive.component'; //Segundo video
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactComponent,
+    ContactReactiveComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule, //Primer video
+    ReactiveFormsModule,//Segundo video
   ],
   providers: [],
   bootstrap: [AppComponent]
